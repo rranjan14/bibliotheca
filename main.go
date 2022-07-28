8e77f6c27333e1a93e42cca34a27f625c02fc379
Rahul Ranjan <rranjan18bcs@iiitkottayam.ac.in>
2022-07-28 11:28:29 +0530

    added db and more tests

--- main.go
+++ main.go
@@ -1,26 +1,80 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	ID     uint
+	Title  string
+	Author string
+}
+
+func connectDatabase(db *gorm.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Set("database", db)
+	}
+}
+
+func setupDatabase(db *gorm.DB) error {
+	err := db.AutoMigrate(
+		&Book{},
 	)
-	
-func defaultHandler(c *gin.Context){
-	c.HTML(http.StatusOK,"default.html",gin.H{})
+	if err != nil {
+		return fmt.Errorf("Error migrating database: %s", err)
+	}
+	return nil
 }
 
-func setupRouter(r *gin.Engine){
-	r.LoadHTMLGlob("templates/**/*.html");
-	r.GET("/",defaultHandler)
+func defaultHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "default.html", gin.H{})
 }
 
-func main(){
+func setupRouter(r *gin.Engine, db *gorm.DB) {
+	r.LoadHTMLGlob("templates/**/*.html")
+	r.Use(connectDatabase(db))
+	r.GET("/books", bookIndexHandler)
+	r.GET("/", defaultHandler, func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/books")
+	})
+}
+
+func bookIndexHandler(c *gin.Context) {
+	db := c.Value("database").(*gorm.DB)
+	books := []Book{}
+	if err := db.Find(&books).Error; err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.HTML(http.StatusOK, "books/index.html", gin.H{"books": books})
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("bibliotheca.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %s", err)
+	}
+
+	err = setupDatabase(db)
+
+	if err != nil {
+		log.Fatalf("Database setup error: %s", err)
+	}
+
 	r := gin.Default()
-	setupRouter(r)
-	err := r.Run(":8000")
-	if err != nil{
+
+	setupRouter(r, db)
+
+	err = r.Run(":8000")
+
+	if err != nil {
 		log.Fatalf("gin Run error: %s", err)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -5,34 +5,74 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
-	
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func TestDefaultRoute(t *testing.T){
+func freshDb(t *testing.T, path ...string) *gorm.DB {
+	t.Helper()
+
+	var dbUri string
+
+	// Note: path can be specified in an individual test for debugging
+	// purposes -- so the db file can be inspected after the test runs.
+	// Normally it should be left off so that a truly fresh memory db is
+	// used every time.
+	if len(path) == 0 {
+		dbUri = ":memory:"
+	} else {
+		dbUri = path[0]
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbUri), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("Error opening memory db: %s", err)
+	}
+	if err := setupDatabase(db); err != nil {
+		t.Fatalf("Error setting up db: %s", err)
+	}
+	return db
+}
+
+// This tests that a fresh database returns no rows (but no error) when
+// fetching Books.
+func TestBookEmpty(t *testing.T) {
+	db := freshDb(t)
+	books := []Book{}
+	if err := db.Find(&books).Error; err != nil {
+		t.Fatalf("Error querying books from fresh db: %s", err)
+	}
+	if len(books) != 0 {
+		t.Errorf("Expected 0 books, got %d", len(books))
+	}
+}
+
+func TestDefaultRoute(t *testing.T) {
 	t.Parallel()
-	
-	w:= httptest.NewRecorder()
-	
-	ctx,r := gin.CreateTestContext(w)
-	
-	setupRouter(r)
-	
+
+	w := httptest.NewRecorder()
+
+	ctx, r := gin.CreateTestContext(w)
+
+	setupRouter(r, freshDb(t))
+
 	req, err := http.NewRequestWithContext(ctx, "GET", "/", nil)
 	if err != nil {
 		t.Errorf("got error: %s", err)
 	}
-	
+
 	r.ServeHTTP(w, req)
 	if http.StatusOK != w.Code {
 		t.Fatalf("expected response code %d, got %d", http.StatusOK, w.Code)
 	}
-	
+
 	body := w.Body.String()
-	
-	expected := "Hello Gin!"
-	
-	if expected != strings.Trim(body, " \r\n") {
+
+	expected := "<h2>My Books</h2>"
+
+	if !strings.Contains(body, expected) {
 		t.Fatalf("expected response body '%s', got '%s'", expected, body)
 	}
 }
